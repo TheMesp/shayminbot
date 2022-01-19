accf22c95d1fb3f39705ad1a2b72b62ba363b394
root <dloewen481@gmail.com>
2022-01-19 04:31:54 +0000

    Added mutex support for shaymin

--- bot.rb
+++ bot.rb
@@ -18,37 +18,47 @@ bot.command(:hornets, description: 'You know it', usage: 's!hornets') do |event|
 end
 
 bot.command(:quotes) do |event, *filter|
-    filter = filter.join(' ')
-    event.respond("Scanning quotes, this can take 15-30 seconds...")
-    quotes_channel = event.server.channels.select{|channel| channel.id == 746958219368202310}
-    quotes_channel = quotes_channel.pop
-    quotes = []
-    next_id = nil
-    loop do
-        chunk = quotes_channel.history(100, next_id)
-        next_id = chunk.last.id
-        quotes += chunk
-        print "chunk length: #{quotes.length}\n"
-        if chunk.length < 100
-            break
-        end 
-    end
-    quotes = quotes.select{|quote| quote.embeds[0]&.author&.name&.include? filter} if filter
-    if quotes.length > 0
-        quotes = quotes.sort_by { |quote| -quote.content.split("#")[1].to_i }
-        description = "Top 10 quotes"
-        description += " by #{filter}" if filter
-        description += "\n"
-        quotes.first(10).each do |quote|
-            description += "#{quote.embeds[0]&.author&.url} by **#{quote.embeds[0]&.author&.name}**\n"
+    unless @mutex
+        @mutex = true
+        filter = filter.join(' ')
+        event.respond("Scanning quotes, this can take 15-30 seconds...")
+        quotes_channel = event.server.channels.select{|channel| channel.id == 746958219368202310}
+        quotes_channel = quotes_channel.pop
+        quotes = []
+        next_id = nil
+        loop do
+            chunk = quotes_channel.history(100, next_id)
+            next_id = chunk.last.id
+            quotes += chunk
+            print "chunk length: #{quotes.length}\n"
+            if chunk.length < 100
+                break
+            end 
         end
-        event.respond(description)
+        quotes = quotes.select{|quote| quote.embeds[0]&.author&.name&.include? filter} if filter
+        if quotes.length > 0
+            quotes = quotes.sort_by { |quote| -quote.content.split("#")[1].to_i }
+            description = "Top 10 quotes"
+            description += " by #{filter}" if filter
+            description += "\n"
+            quotes.first(10).each do |quote|
+                description += "#{quote.embeds[0]&.author&.url} by **#{quote.embeds[0]&.author&.name}**\n"
+            end
+            @mutex = false
+            event.respond(description)
+        else
+            @mutex = false
+            event.respond("Didn't find any quotes by #{filter.gsub('@','')}. I check what you give as a filter against what Quotebot has as the username, so take a look at that to see if it matches.")
+        end
+        
     else
-        event.respond("Didn't find any quotes by #{filter}. I'm checking exactly what quotebot says is the user, so make sure you're matching that.")
+        event.respond "Please wait your turn."
     end
-    
 end
 
+bot.command(:unlock) do |event|
+    @mutex = false
+end
 bot.command(:echo, description: 'Have shaymin say whatever you say', usage: 's!echo <optional: channel to echo in> <text to be repeated>', min_args: 1) do |event, *text|
 	if text[0] =~ /<#\d{2,}>/
 		channel = text.shift
@@ -103,6 +113,7 @@ bot.command(:praise, description: 'generate a kind compliment', usage: 's!praise
 end
 
 # initial setup
+@mutex = false
 bot.run(true)
 puts 'bot active'
 bot.join
